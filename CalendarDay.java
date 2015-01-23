6a2d15843ab8c68cd51d8e7b3a076c03d4b3a273
Aaron <aslaugh1@gmail.com>
2015-01-23 10:33:43 -0500

    Update CalendarDay.java

--- CalendarDay.java
+++ CalendarDay.java
@@ -1,9 +1,5 @@
 package xcom.log;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Date;
-
 public class CalendarDay {
 
 	public static final int MAX_EVENTS_PER_DAY = 20; 
@@ -35,23 +31,6 @@ public class CalendarDay {
 		}
 		
 		return i;
-	}
-	
-	public static void main(String args[]) {
-	Calendar myCalendar = new GregorianCalendar();
-		
-		System.out.println(myCalendar.getTime().toString());
-
-		myCalendar.clear();
-		myCalendar.set(2015, 2, 12);
-		System.out.println(myCalendar.getTime());
-		System.out.println(myCalendar.get(Calendar.MONTH));
-		
-		Date myDate = new Date();
-		
-		System.out.println(myDate.toString());
-		
-		
 	}
 
 }
