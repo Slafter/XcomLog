144570416bcf493468f0862ecb5c5c8948d33e94
Aaron <aslaugh1@gmail.com>
2015-01-24 17:49:54 -0500

    Update CalendarSheet.java

--- CalendarSheet.java
+++ CalendarSheet.java
@@ -1,10 +1,13 @@
 package xcom.log;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class CalendarSheet {
 	
 	public static final int MAX_CALENDAR_DAYS = 1500;
+	private static final Calendar startDate = new GregorianCalendar(2015, 3, 1);
+	private static final long millisecondsPerDay = 86400000;
 	
 	private CalendarDay[] calendar = new CalendarDay[MAX_CALENDAR_DAYS];
 	
@@ -14,17 +17,33 @@ public class CalendarSheet {
 		}
 	}
 	
-	public void addEventAtDay(Event event, int day) {
-		calendar[day].addEvent(event);
+	public void addEvent(Event event, int index) {
+		calendar[index].addEvent(event);
+	}
+	
+	public void addEvent(Event event, Calendar date) {
+		int index;
+	
+		index = convertCalendarToArrayIndex(date);
+		addEvent(event, (int)index);
 	}
 	
 	public Event getEvent(int day, int index) {
 		return calendar[day].getEventAtIndex(index);
 	}
 	
-	private int convertDateToCalendarIndex(Date date) {
-		// TODO
-		return 0;
+	public Event getEvent(Calendar day, int index) {
+		return getEvent(convertCalendarToArrayIndex(day), index);
+	}
+	
+	private int convertCalendarToArrayIndex(Calendar calendar) {
+		long index = 0;
+		long milliseconds;
+		
+		milliseconds = calendar.getTimeInMillis() - startDate.getTimeInMillis();
+		index = milliseconds / millisecondsPerDay;
+		
+		return (int)index;
 	}
 	
 }
